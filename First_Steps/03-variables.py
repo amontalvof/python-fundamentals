92b6b6f6f7f950d33b2280db001716c484fe881a
amontalvof <amontalvof92@gmail.com>
2026-03-21 12:56:34 -0400

    variables

---
+++ First_Steps/03-variables.py
@@ -0,0 +1,12 @@
+name = 'Ricardo'
+lastname = 'Cuellar'
+age = 20
+money = 12
+complete_name = 'Ricardo Cuellar'
+PI = 3.14
+print(name)
+print(lastname)
+print(age)
+print(money)
+print(complete_name)
+print(PI)
