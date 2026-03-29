1387172173fc67988ebc4db7969a5f3a714bf7d2
amontalvof <amontalvof92@gmail.com>
2026-03-29 10:59:25 -0400

    Basic Data Types

---
+++ First_Steps/04-data_types.py
@@ -0,0 +1,34 @@
+# String
+name = 'Andy'
+
+# Int
+age=30
+
+# Float
+temperature = 27.3
+
+# Boolean
+is_developer = True
+
+# List (Arrays)
+my_list = [1,2,3]
+
+# Tuple
+my_tuple = (1,2,3)
+
+# Set
+my_set = {1,2,3}
+
+# Dictionary
+my_dictionary = {
+    'hello':'hola',
+    'bye':'adios'
+}
+
+print(name)
+print(age)
+print(temperature)
+print(is_developer)
+print(my_list)
+print(my_tuple)
+print(my_dictionary)
