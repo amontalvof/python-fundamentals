1ebee67f2b45c94301e767052057e008b6cef34b
amontalvof <amontalvof92@gmail.com>
2026-04-11 14:22:19 -0400

    Text strings

---
+++ First_Steps/05-strings.py
@@ -0,0 +1,15 @@
+text1 = 'Hola'
+text2 = "Mundo"
+text3 = '''
+Hola
+Mundo'''
+
+print(text1)
+print(text2)
+print(text3)
+
+# String concatenation
+name = 'Andy'
+lastName = 'Smith'
+fullName = name + ' ' + lastName
+print(fullName)
