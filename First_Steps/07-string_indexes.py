b1226818cec16c37d70d42abd6edea41f658b7d4
amontalvof <amontalvof92@gmail.com>
2026-04-26 11:34:16 -0400

    String Indexes

---
+++ First_Steps/07-string_indexes.py
@@ -0,0 +1,12 @@
+name = "Fernandez"
+last_letter = name[-1]
+middle_letter = name[1:8]
+middle_letter_step = name[1:8:2]
+
+for i in range(len(name)):
+    print(name[i])
+
+
+print('last_letter:', last_letter)
+print("middle_letter:", middle_letter)
+print("middle_letter_step:", middle_letter_step)
