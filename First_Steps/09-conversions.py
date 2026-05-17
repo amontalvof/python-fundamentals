b3f793d2f0e88d9fb14f103ae7b90d6f534e4cb5
amontalvof <amontalvof92@gmail.com>
2026-05-17 13:23:00 -0400

    Conversions

---
+++ First_Steps/09-conversions.py
@@ -0,0 +1,19 @@
+value = 100
+type_value = type(value)
+print(value)
+print(type_value)
+
+value_str = str(value)
+type_value_str = type(value_str)
+print(value_str)
+print(type_value_str)
+
+value_int = int(value_str)
+type_value_int = type(value_int)
+print(value_int)
+print(type_value_int)
+
+value_float = float(value_str)
+type_value_float = type(value_float)
+print(value_float)
+print(type_value_float)
