b6cd623ad98091c52fa5c6392639c53531a4f1f9
amontalvof <amontalvof92@gmail.com>
2026-05-23 09:25:15 -0400

    Basic Mathematical Operators

---
+++ First_Steps/10-math_operations.py
@@ -0,0 +1,9 @@
+print(5+2)  # Addition 7
+print(5-2)  # Subtraction 3
+print(5*2)  # Multiplication 10
+print(5**2)  # Exponentiation 25
+print(5/2)  # Division 2.5
+print(5//2)  # Floor Division 2
+print(5 % 2)  # Modulo 1
+print(round(5/2))  # Rounding 2
+print(abs(-5))  # Absolute value 5
