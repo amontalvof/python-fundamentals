b8c08811437aa43d47005e0984f6ee8dea4876cf
amontalvof <amontalvof92@gmail.com>
2026-05-25 08:13:46 -0400

    Add built-in and method functions examples

---
+++ First_Steps/11-incorporated_fuctions.py
@@ -0,0 +1,13 @@
+# Built-in functions
+print("Hello World")  # Output: Hello World
+type("Hello World")  # <class 'str'>
+str(123)  # '123'
+round(20.9)  # 123
+abs(-5)  # 5
+
+# Method functions
+message = "Hello World"
+print(message.upper())  # Output: HELLO WORLD
+print(message.lower())  # Output: hello world
+print(message.split())  # Output: ['Hello', 'World']
+print(message.replace("World", "Python"))  # Output: Hello Python
